ce23a1df3cf5f1c3c929532ff24874a4473ff4e3
FJFontanarrosa <franciscof85@gmail.com>
2022-08-21 19:01:36 -0300

    Primer push de prueba

---
+++ concesionaria.js
@@ -0,0 +1,90 @@
+let autos = require('./Modulos/autos.js');
+
+let persona = {
+    nombre: "Juan",
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000
+ }
+
+let concesionaria = {
+   autos: autos,
+   
+   buscarAuto: function(patenteEntrada){
+        for(let x of autos){
+         if (x.patente == patenteEntrada){
+
+            return x;
+         }
+
+      }
+      return null;
+   },
+
+    venderAuto:function(patente){
+       let autoSearch = this.buscarAuto(patente);
+       if(autoSearch !== null){
+            autoSearch.vendido = true;
+       }
+            return autoSearch;
+    },
+   
+       autosParaLaVenta: function(){
+         let autosParaLaVenta = this.autos.filter(function(e){
+           return e.vendido !== true;
+       })
+       return autosParaLaVenta;
+    }, 
+         autosNuevos: function (){
+            return this.autosParaLaVenta().filter(autos=> ((autos.km < 100)&&(autos.vendido == false)))
+   },
+
+         listaDeVentas: function(){
+            let listaDeVentas = [ ];
+            for(i=0;i<this.autos.length;i++){
+               if(this.autos[i].vendido == true){
+                  listaDeVentas.push(this.autos[i].precio)
+                }
+            }
+               return listaDeVentas;
+    },
+
+         totalDeVentas:function(){ //Con el reduce, si el array esta vacio arroja error!! En ese caso, va esta sintaxis
+         let valorInicial = 0;
+         let totalDeVentas = this.listaDeVentas().reduce((total,valor)=>total +valor,valorInicial);
+            return totalDeVentas;
+    },
+
+         puedeComprar:function(auto, persona){
+
+             if (auto.precio > persona.capacidadDePagoTotal){
+            return false;
+         }
+            let precioPorCuota = auto.precio / auto.cuotas;
+            if (precioPorCuota > persona.capacidadDePagoEnCuotas) {
+          return false;
+      }
+      return true;
+    },
+
+         autosQuePuedeComprar:function(persona){
+           let autosDisponibles = this.autos.autosParaLaVenta();
+           
+
+    },
+
+         autosQuePuedeComprar:function(persona) {
+            let disponibles = this.autosParaLaVenta();
+            let lista=[]
+         disponibles.filter(autos=>{
+         if (this.puedeComprar(autos,persona)) {
+         lista.push(this.puedeComprar)
+      }
+      })
+      return lista
+   },
+}
+
+concesionaria.autosQuePuedeComprar(persona)
+
+
+   
